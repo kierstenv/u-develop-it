928652a1f4af61d035e6b456ecfa8adae55adf17
kierstenv <kierstenvicknair22@gmail.com>
2022-03-31 15:01:32 -0500

    connect to mySQL db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,11 +7,15 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World !'
-  });
-});
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'Ro0tsUx22!1!',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 app.use((req, res) => {
   res.status(404).end();
